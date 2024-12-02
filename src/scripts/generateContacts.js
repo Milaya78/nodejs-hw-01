73ad26867b729227d6fc655b1a3a824e789d86b2
Milaya78 <ela_keyt@mail.ru>
2024-12-02 19:24:28 +0100

    init node

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,15 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+console.log(fs);
+
+const generateContacts = async (number) => {
+  const contactList = await readContacts();
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+  const newList = [...contactList, ...newContacts];
+  await writeContacts(newList);
+};
 
 generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,10 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts';
+import { writeContacts } from '../utils/writeContacts';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  contacts.pop();
+  await writeContacts(contacts);
+};
 
 removeLastContact();
